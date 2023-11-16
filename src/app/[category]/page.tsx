becea601467443037b599d38a2c6e100537b6edb
ajin <aj5210@naver.com>
2023-11-16 20:43:17 +0900

    feat: 페이지 전환 에니메이션 추가

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,9 +1,9 @@
 'use client'
-
 import { usePathname } from 'next/navigation';
 import Navbar from '../components/Navbar';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const categoryDetails: CategoryDetail = {
+const categoryDetails = {
     music: {
         backgroundColor: '#FFFFFF',
         pointColor: '#0466C8'
@@ -20,7 +20,7 @@ const categoryDetails: CategoryDetail = {
 
 export default function CategoryDetail() {
     const pathname = usePathname();
-    const category = pathname.replace('/','') as Category;
+    const category = pathname.replace('/', '') as keyof typeof categoryDetails;
 
     const categoryDetail = categoryDetails[category];
 
@@ -29,11 +29,27 @@ export default function CategoryDetail() {
     }
 
     return (
-        <div>
-            <div style={{ backgroundColor: categoryDetail.backgroundColor, color: categoryDetail.pointColor }}>
-                <Navbar curpath={pathname} pointColor={categoryDetail.pointColor}/>
-                <div className='h-screen'>{category} page</div>
-            </div>
-        </div>
+        <AnimatePresence>
+            <motion.div
+                key={pathname}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+                style={{ backgroundColor: categoryDetail.backgroundColor, color: categoryDetail.pointColor }}
+            >
+                <Navbar curpath={pathname} pointColor={categoryDetail.pointColor} />
+                <motion.div
+                    key={category}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -50 }}
+                    transition={{ duration: 0.5 }}
+                    className='h-screen'
+                >
+                    {category} page
+                </motion.div>
+            </motion.div>
+        </AnimatePresence>
     );
-};
+}
