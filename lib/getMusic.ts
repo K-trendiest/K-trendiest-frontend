3ce5d41fe16aec84ba05ba7acaa8ab743d359047
ajin <aj5210@naver.com>
2024-01-31 23:26:21 +0900

    배포 테스트2

--- lib/getMusic.ts
+++ lib/getMusic.ts
@@ -1,5 +1,5 @@
 export default async function getMusic(): Promise<TrendItem[]> {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/musics`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/musics`, {
     next: { revalidate: 60 },
   })
 
