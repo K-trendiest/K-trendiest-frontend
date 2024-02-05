abba00bb073cc25761556277eb268b5c67376def
ajin <aj5210@naver.com>
2024-02-05 15:14:45 +0900

    배포,... 해줘

--- lib/getMovie.ts
+++ lib/getMovie.ts
@@ -1,13 +1,13 @@
 import { BASE_API_URL } from '@/utils/constants'
 
 export default async function getMovie() {
-  const res = await fetch(`${BASE_API_URL}/api/movies`, {
-    next: { revalidate: 60 },
-  })
-
   if (!BASE_API_URL) {
     return null
   }
+
+  const res = await fetch(`${BASE_API_URL}/api/movies`, {
+    next: { revalidate: 60 },
+  })
 
   if (!res.ok) throw new Error('failed to fetch MOVIE data')
 
--- lib/getYoutube.ts
+++ lib/getYoutube.ts
@@ -1,13 +1,13 @@
 import { BASE_API_URL } from '@/utils/constants'
 
 export default async function getMusic() {
-  const res = await fetch(`${BASE_API_URL}/api/youtube`, {
-    next: { revalidate: 60 },
-  })
-
   if (!BASE_API_URL) {
     return null
   }
+
+  const res = await fetch(`${BASE_API_URL}/api/youtube`, {
+    next: { revalidate: 60 },
+  })
 
   if (!res.ok) throw new Error('failed to fetch YOUTUBE data')
 
